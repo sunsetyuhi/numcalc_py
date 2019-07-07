2d9c65f3d18a311142c8c5d086252091da4d1332
sunsetyuhi <48707395+sunsetyuhi@users.noreply.github.com>
2019-07-07 19:25:09 +0900

    Update runge_kutta2.py

--- ordinary_differential_equation/runge_kutta2/runge_kutta2.py
+++ ordinary_differential_equation/runge_kutta2/runge_kutta2.py
@@ -68,7 +68,7 @@ def visualization(t_list, y_list):
 
 
 #メイン実行部
-if __name__ == '__main__':
+if (__name__ == '__main__'):
     #2次のRunge-Kutta法でdt離れた点の値を取得（導関数、tの初期値、yの初期値、刻み幅dt）
     t, y = runge_kutta2(func_dydt, 0.0, 0.0, 1e-1)
     print("t = {:.7f},  y = {:.7f}".format(t, y))
