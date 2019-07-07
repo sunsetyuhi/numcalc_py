7fe0929723a31433e66e0daab5d7658ffd6d8957
sunsetyuhi <48707395+sunsetyuhi@users.noreply.github.com>
2019-07-07 17:15:43 +0900

    Create mod_test.py

---
+++ ordinary_differential_equation/euler/mod_test.py
@@ -0,0 +1,16 @@
+import numpy as np  #数値計算用モジュール
+import euler
+
+
+def func_dydt1(t, y):
+    return np.cos(t)  #dy/dt = cos(t)
+
+t_list1, y_list1 = euler.ode_calc(func_dydt1, 0.0, 5.0, -5.0, 1e-1)
+euler.visualization(t_list1, y_list1)
+
+
+def func_dydt2(t, y):
+    return -y  #dy/dt = -y(t)
+
+t_list2, y_list2 = euler.ode_calc(func_dydt2, 5.0, 0.0, 10.0, 1e-1)
+euler.visualization(t_list2, y_list2)
