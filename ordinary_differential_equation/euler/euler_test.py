949652c4c9be584fc93139237bb15c2f6e4ebb54
sunsetyuhi <48707395+sunsetyuhi@users.noreply.github.com>
2019-07-30 11:36:35 +0900

    Update and rename mod_test.py to euler_test.py

--- ordinary_differential_equation/euler/euler_test.py
+++ ordinary_differential_equation/euler/euler_test.py
@@ -5,12 +5,12 @@ import euler
 def func_dydt1(t, y):
     return np.cos(t)  #dy/dt = cos(t)
 
-t_list1, y_list1 = euler.ode_calc(func_dydt1, 0.0, 5.0, -5.0, 1e-1)
+t_list1, y_list1 = euler.ode_calc(func_dydt1, 0.0, 5.0, -5.0, dt=1e-1)
 euler.visualization(t_list1, y_list1)
 
 
 def func_dydt2(t, y):
     return -y  #dy/dt = -y(t)
 
-t_list2, y_list2 = euler.ode_calc(func_dydt2, 5.0, 0.0, 10.0, 1e-1)
+t_list2, y_list2 = euler.ode_calc(func_dydt2, 5.0, 0.0, 10.0)
 euler.visualization(t_list2, y_list2)
