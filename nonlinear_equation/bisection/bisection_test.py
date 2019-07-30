9e241f366ac4477cfc8943c2d473ab78720baa37
sunsetyuhi <48707395+sunsetyuhi@users.noreply.github.com>
2019-07-30 11:15:59 +0900

    Update and rename mod_test.py to bisection_test.py

--- nonlinear_equation/bisection/bisection_test.py
+++ nonlinear_equation/bisection/bisection_test.py
@@ -5,12 +5,12 @@ import bisection
 def func_f1(x):
     return x**2.0 -3.0
 
-solution1 = bisection.bisection(func_f1, 1.0, 2.0, 1e-15)
+solution1 = bisection.bisection(func_f1, 1.0, 2.0, error=1e-15)
 bisection.visualization(func_f1, solution1-1.0, solution1+1.0, solution1)
 
 
 def func_f2(x):
     return np.arctan(x)
 
-solution2 = bisection.bisection(func_f2, -1.0, 1.0, 1e-10)
+solution2 = bisection.bisection(func_f2, -1.0, 1.0)
 bisection.visualization(func_f2, solution2-1.0, solution2+1.0, solution2)
