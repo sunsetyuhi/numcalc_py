fd78e55eda996199b268f4eddf43437857550c03
sunsetyuhi <48707395+sunsetyuhi@users.noreply.github.com>
2019-07-30 11:20:58 +0900

    Update and rename mod_test.py to newton_test.py

--- nonlinear_equation/newton/newton_test.py
+++ nonlinear_equation/newton/newton_test.py
@@ -5,12 +5,12 @@ import newton
 def func_f1(x):
     return x**2.0 -3.0
 
-solution1 = newton.newton(func_f1, 1.0, 1e-15)
+solution1 = newton.newton(func_f1, 1.0, error=1e-15)
 newton.visualization(func_f1, solution1-1.0, solution1+1.0, solution1)
 
 
 def func_f2(x):
     return np.arctan(x)
 
-solution2 = newton.newton(func_f2, 2.0, 1e-10)
+solution2 = newton.newton(func_f2, 2.0)
 newton.visualization(func_f2, solution2-1.0, solution2+1.0, solution2)
